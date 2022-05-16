4c6c47fb094ca98ec12cf7cec15eb6f3468bfb21
Venkatesh Kumar <venkateshkumartuty@gmail.com>
2022-05-16 20:03:56 +0530

    Add project files.

---
+++ PensionerDetail/Migrations/20220508164701_NewPensionerDetails.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PensionerDetail.Migrations
+{
+    public partial class NewPensionerDetails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Pensioners",
+                newName: "PensionType");
+
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Pensioners",
+                newName: "AadharNumber");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PensionType",
+                table: "Pensioners",
+                newName: "UserName");
+
+            migrationBuilder.RenameColumn(
+                name: "AadharNumber",
+                table: "Pensioners",
+                newName: "Type");
+        }
+    }
+}
---
+++ ProcessPension/Migrations/20220512183053_InitialProcessPension.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProcessPension.Migrations
+{
+    public partial class InitialProcessPension : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "processes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AadharNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Salary = table.Column<double>(type: "float", nullable: false),
+                    Allowance = table.Column<double>(type: "float", nullable: false),
+                    PensionType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BankType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ResultAmount = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_processes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "processes");
+        }
+    }
+}
